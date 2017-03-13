084bfa0cec1dcf66178e0f3b9782c9bef7139162
Tom Leonardsson <tom.leo@hotmail.com>
2017-03-13 21:02:53 +0100

    can load users into binary search tree

--- project/src/collections/User.java
+++ project/src/collections/User.java
@@ -6,6 +6,12 @@ public class User {
 	private String phoneNumber;
 	
 	public User(String id, String name, String phoneNumber) {
-		
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String toString() {
+		return "{" + id + ", " + name + ", " + phoneNumber + "}";
 	}
 }
--- project/src/library/LibraryController.java
+++ project/src/library/LibraryController.java
@@ -7,9 +7,28 @@ import collections.Book;
 import collections.Dvd;
 import collections.Hashtable;
 import collections.Media;
+import collections.User;
+import collections.BinarySearchTree;
 
 public class LibraryController {
 	private Hashtable<String, Media> media;
+	private BinarySearchTree<String, User> users;
+	
+	public void loadUsers(String path) {
+		users = new BinarySearchTree<String, User>();
+		String[] values = new String[0];
+		int counter = 0;
+		try {
+			values = ResourceReader.getValuesOnLine(path, counter);
+			while(values != null) {
+				values = ResourceReader.getValuesOnLine(path, counter);
+				if(values != null) users.put(values[0], new User(values[0], values[1], values[2]));
+				counter += 1;
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
 	
 	public int getAmountOfMedia(String path) {
 		String[] values = new String[0];
@@ -49,5 +68,9 @@ public class LibraryController {
 		} catch (IOException e) {
 			System.out.println(e);
 		}
+	}
+	
+	public static void main(String[] args) {
+		new LibraryController().loadUsers("data/Lantagare");
 	}
 }
