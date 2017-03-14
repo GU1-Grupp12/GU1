ec44efa5558ce34ee5f3bd8a0cdf7c111c785df2
Tom Leonardsson <tom.leo@hotmail.com>
2017-03-14 11:01:52 +0100

    borrwoed media

--- project/src/collections/User.java
+++ project/src/collections/User.java
@@ -5,10 +5,18 @@ public class User {
 	private String name;
 	private String phoneNumber;
 	
+	private List<Media> borrowedMedia;
+	
 	public User(String id, String name, String phoneNumber) {
 		this.id = id;
 		this.name = name;
 		this.phoneNumber = phoneNumber;
+		
+		borrowedMedia = new List<Media>();
+	}
+	
+	public List<Media> getBorrowedMedia() {
+		return borrowedMedia;
 	}
 	
 	public String toString() {
