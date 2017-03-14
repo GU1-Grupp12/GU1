c61c3d4c902e86fe3463e19494ed80a5c06daac8
Radsko <igniter51@gmail.com>
2017-03-14 23:58:42 +0100

    BorrowView Färdig

--- project/src/library/BorrowView.java
+++ project/src/library/BorrowView.java
@@ -3,49 +3,61 @@ package library;
 import java.awt.*;
 import javax.swing.*;
 
-public class BorrowView extends JPanel{
+public class BorrowView extends JPanel {
 	private BorrowController controller;
-	
-	private JButton lona = new JButton("Låna");
+
 	private JTextField mediaId = new JTextField();
-	private JLabel mId = new JLabel("Media ID: ");
-	private List list = new List();
-	private JPanel sokField = new JPanel();
 	private JTextField sok = new JTextField();
+
+	private JLabel mId = new JLabel("Media ID: ");
 	private JLabel sokf = new JLabel("Sök");
+
+	private List list = new List();
+
 	private JPanel lonField = new JPanel();
-	private JFrame frame;
-	
-	public BorrowView (BorrowController controller){
+	private JPanel btnField = new JPanel();
+
+	private JButton lona = new JButton("Låna");
+	private JButton logout = new JButton("logga ut");
+	private JButton sokMed = new JButton("Sök");
+
+	public BorrowView(BorrowController controller) {
 		this.controller = controller;
-		
-		
+
 		setLayout(new BorderLayout());
-		lonField.setLayout(new GridLayout(0,2));
-		sokField.setLayout(new GridLayout(0,2));
+		lonField.setLayout(new GridLayout(2, 2));
+		btnField.setLayout(new GridLayout(10, 0));
+
 		mId.setPreferredSize(new Dimension(80, mId.getSize().height));
-		sokf.setPreferredSize(new Dimension(80,sokf.getSize().height));
-		
-		sokField.add(sok);
-		sokField.add(sokf);
-		
+		sokf.setPreferredSize(new Dimension(80, sokf.getSize().height));
+
+		btnField.add(sokMed);
+		btnField.add(lona);
+		btnField.add(logout);
+
+		sok.setToolTipText("Mata in media-ID");
+		mediaId.setToolTipText("Mata in media -ID");
+
+		lonField.add(sokf);
+		lonField.add(sok);
 		lonField.add(mId);
 		lonField.add(mediaId);
-		frame.add(lonField, BorderLayout.NORTH);
-		frame.add(sokField, BorderLayout.NORTH);
-		frame.add(list, BorderLayout.WEST);
-		frame.add(lona, BorderLayout.CENTER);
-		frame.add(lona, BorderLayout.EAST);
-		
-		
+
+		this.add(btnField, BorderLayout.EAST);
+		this.add(lonField, BorderLayout.NORTH);
+		this.add(list, BorderLayout.WEST);
+
 	}
+
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setTitle("Spacebar Invader");
+		BorrowController controller = new BorrowController();
+		BorrowView bucky = new BorrowView(controller);
+		frame.setTitle("Borrow");
 		frame.setSize(600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible( true );
+		frame.add(bucky);
+		frame.setVisible(true);
 	}
-	
 
 }
