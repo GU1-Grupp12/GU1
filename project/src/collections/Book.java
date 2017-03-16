1e02b1b725783f821ae19ba7ccf35beb556f4878
Carl Zetterberg <caze1996@gmail.com>
2017-03-16 14:39:26 +0100

    comments
    dön wid book and började på dvd

--- project/src/collections/Book.java
+++ project/src/collections/Book.java
@@ -18,17 +18,25 @@ public class Book extends Media{
 	 * @param id the id of the book
 	 * @param title of the book
 	 * @param year that the book was released
-	 * @param author that wrote 
+	 * @param author that wrote the book
 	 */
 	public Book(String id, String title, int year, String author) {
 		super(id, title, year);
 		this.author = author;
 	}
-	
+	/**
+	 * gets the name of the author of the book
+	 * 
+	 * @return authors name
+	 */
 	public String getAuthor(){
 		return author;	
 	}
-	
+	/**
+	 *Creates a string that explains the book object 
+	 * 
+	 * @return a string 
+	 */
 	public String toString() {
 		String tmp = "{";
 		tmp += getId() + ", " + getTitle() + ", " + getYear() + ", " + author;
